98c27683a48d854b4c9718778ca83d6cdd80f67a
Ar gun <L_E_R_A02@mail.ru>
2022-10-09 16:33:23 +0300

    first commit

---
+++ 03.10.22/2 algoritmi.py
@@ -0,0 +1,8 @@
+def num(n):
+    if n % 2 ==2:
+        return n
+    else:
+        return n*2
+print(num(5))
+
+
---
+++ 21.09.22/3.py
@@ -0,0 +1,3 @@
+numbers = [5,3,2,4,5,5,5,4,2,3,4,5,5,5,]
+print(numbers.count(5))
+
